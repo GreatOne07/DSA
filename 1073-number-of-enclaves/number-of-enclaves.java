2438eb3299e5c314a21c1e4e508d8f184af34564
Ansh Srivastava <ansh1311srivastava@gmail.com>
2026-07-23 00:00:19 +0530

    Time: 8 ms (69.32%) | Memory: 60.7 MB (47.90%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,67 @@
+class Solution {
+
+    public void dfs(int row, int col, int[][] vis, int[][] grid, int[] delrow, int[] delcol) {
+
+        vis[row][col] = 1;
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        for (int i = 0; i < 4; i++) {
+
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+
+            if (nrow >= 0 && nrow < n &&
+                ncol >= 0 && ncol < m &&
+                vis[nrow][ncol] == 0 &&
+                grid[nrow][ncol] == 1) {
+
+                dfs(nrow, ncol, vis, grid, delrow, delcol);
+            }
+        }
+    }
+
+    public int numEnclaves(int[][] grid) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int[][] vis = new int[n][m];
+
+        int[] delrow = {-1, 0, 1, 0};
+        int[] delcol = {0, 1, 0, -1};
+
+        // First and last row
+        for (int j = 0; j < m; j++) {
+
+            if (grid[0][j] == 1 && vis[0][j] == 0)
+                dfs(0, j, vis, grid, delrow, delcol);
+
+            if (grid[n - 1][j] == 1 && vis[n - 1][j] == 0)
+                dfs(n - 1, j, vis, grid, delrow, delcol);
+        }
+
+        // First and last column
+        for (int i = 0; i < n; i++) {
+
+            if (grid[i][0] == 1 && vis[i][0] == 0)
+                dfs(i, 0, vis, grid, delrow, delcol);
+
+            if (grid[i][m - 1] == 1 && vis[i][m - 1] == 0)
+                dfs(i, m - 1, vis, grid, delrow, delcol);
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1 && vis[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
