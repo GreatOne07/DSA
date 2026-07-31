213442d1764f184d46c0eaa41781f9935f591dca
Ansh Srivastava <ansh1311srivastava@gmail.com>
2026-07-31 23:52:51 +0530

    Time: 5 ms (82.18%) | Memory: 46.2 MB (98.74%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,58 +1,35 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj =new ArrayList<>();
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        for(int[] pre:prerequisites){
+        int[] indegree = new int [numCourses];
+        for(int[]pre : prerequisites){
             adj.get(pre[1]).add(pre[0]);
+            indegree[pre[0]]++;
         }
-        int vis[]=new int[numCourses];
-        int[] pathVis = new int[numCourses];
-        Stack<Integer> st=new Stack<>();
-        for (int i = 0; i < numCourses; i++) {
-
-            if (vis[i] == 0) {
-
-                if (dfs(i, adj, vis, pathVis, st)) {
-                    return new int[0];  
+        Queue<Integer> q= new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.offer(i);
+            }
+        }
+        int[] ans= new int[numCourses];
+        int index=0;
+        while(!q.isEmpty()){
+            int node=q.poll();
+            ans[index++]=node;
+            for(int neighbour : adj.get(node)){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0){
+                    q.offer(neighbour);
                 }
             }
         }
-         int[] ans = new int[numCourses];
-        int index = 0;
-
-        while (!st.isEmpty()) {
-            ans[index++] = st.pop();
+        if(index != numCourses){
+            return new int[0];
         }
-
         return ans;
-    }
-     public boolean dfs(int node,
-                       ArrayList<ArrayList<Integer>> adj,
-                       int[] vis,
-                       int[] pathVis,
-                       Stack<Integer> st) {
-
-        vis[node] = 1;
-        pathVis[node] = 1;
-
-        for (int it : adj.get(node)) {
-
-            if (vis[it] == 0) {
-
-                if (dfs(it, adj, vis, pathVis, st)) {
-                    return true;
-                }
-
-            } else if (pathVis[it] == 1) {
-                return true; 
-            }
-        }
-
-        pathVis[node] = 0;
-        st.push(node);
-
-        return false;
     }
 }
