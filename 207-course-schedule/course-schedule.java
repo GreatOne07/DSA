4fdef59106fbf1bcf2529356dd1d67e40cf1aa29
Ansh Srivastava <ansh1311srivastava@gmail.com>
2026-07-31 23:06:07 +0530

    Time: 9 ms (17.43%) | Memory: 47.5 MB (8.43%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,35 +1,37 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<numCourses; i++){
-            adj.add(new ArrayList <>());}
-
-            int[] indegree =new int[numCourses];
-
-            for(int[] edge : prerequisites){
-                int course =edge[0];
-                int prereq = edge[1];
-
-                adj.get(prereq).add(course);
-                indegree[course]++;
+        ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        int m= prerequisites.length;
+        for(int i=0;i<m;i++){
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        int[] indegree = new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            for(int it : adj.get(i)){
+                indegree[it]++;
             }
-            Queue<Integer> q = new LinkedList <>();
-            for(int i=0;i<numCourses;i++){
-                if(indegree[i]==0){
-                    q.offer(i);
-                }}
-                int count=0;
-                while(!q.isEmpty()){
-                    int node=q.poll();
-                    count++;
-                    for(int next : adj.get(node)){
-                        indegree[next]--;
-                        if(indegree[next]==0)
-                        q.offer(next);
-                    }
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.offer(i);
+            }
+        }
+        ArrayList<Integer> topo = new ArrayList<>();
+        while(!q.isEmpty()){
+            int node=q.poll();
+            topo.add(node);
+
+            for(int it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.offer(it);
                 }
-                
-            
-        return count == numCourses;
+            }
+                    }
+                    return topo.size()==numCourses;
     }
 }
