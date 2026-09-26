5cb1b24cff4147407d8aebf297a5a209454d640b
Ansh Srivastava <ansh1311srivastava@gmail.com>
2026-09-26 16:52:17 +0530

    Time: 40 ms (42.99%) | Memory: 94.9 MB (47.51%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String evaluate(String s, List<List<String>> knowledge) {
+        HashMap<String,String> map=new HashMap<>();
+        for(List<String> pair:knowledge){
+            map.put(pair.get(0),pair.get(1));
+        }
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                i++;
+                StringBuilder key=new StringBuilder();
+                while(s.charAt(i)!=')'){
+                    key.append(s.charAt(i));
+                    i++;
+                }
+                ans.append(map.getOrDefault(key.toString(), "?"));
+            }
+            else{
+                ans.append(s.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+}
